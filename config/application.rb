3d68704d34fa6a64e6bf840291c318d79f378f59
Cerisa <cerisa@strangerelics.com>
2016-09-22 20:16:20 -0700

    Fix CORS config

--- config/application.rb
+++ config/application.rb
@@ -26,14 +26,14 @@ module TabletalkApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
-end
 
-config.middleware.insert_before 'Rack::Runtime', 'Rack::Cors' do
-  allow do
-    origins '*'
-    resource '*',
-             headers: :any,
-             methods: [:get, :put, :post, :patch, :delete, :options]
+    config.middleware.insert_before 'Rack::Runtime', 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*',
+        headers: :any,
+        methods: [:get, :put, :post, :patch, :delete, :options]
+      end
+    end
   end
 end
