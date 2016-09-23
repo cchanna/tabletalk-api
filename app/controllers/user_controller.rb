e667f2dd3471440267848cab93452b937e1d75f2
Cerisa <cerisa@strangerelics.com>
2016-09-22 20:16:35 -0700

    Fix request header getting

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,11 +4,11 @@ class UserController < ApplicationController
   def login
     provider = nil
     if Rails.env.test?
-      provider = params[:provider] || "test"
+      provider = params["provider"] || "test"
     else
-      provider = params[:provider] || "google"
+      provider = params["provider"] || "google"
     end
-    @auth = User.login request.headers[:token], provider
+    @auth = User.login request.headers["token"], provider
     return render 'auth_error', status: :unauthorized unless @auth
     @token = Token.create_from @auth
   end
