e740474d3314a63849801d1e583d3605f946e1e8
Cerisa <cerisa@strangerelics.com>
2016-10-05 20:13:52 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006024323) do
+ActiveRecord::Schema.define(version: 20160907044711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "uuid-ossp"
 
   create_table "auths", force: :cascade do |t|
     t.integer  "provider",   default: 0, null: false
@@ -25,14 +24,6 @@ ActiveRecord::Schema.define(version: 20161006024323) do
     t.datetime "updated_at",             null: false
     t.index ["uid", "provider"], name: "index_auths_on_uid_and_provider", using: :btree
     t.index ["user_id"], name: "index_auths_on_user_id", using: :btree
-  end
-
-  create_table "games", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "name",                    null: false
-    t.integer  "type",        default: 0, null: false
-    t.integer  "max_players",             null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
