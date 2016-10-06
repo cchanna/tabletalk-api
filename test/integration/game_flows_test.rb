b11c46bf9fca17d891cbb90362470000ee6e545c
Cerisa <cerisa@strangerelics.com>
2016-10-06 16:54:55 -0700

    Add parenthesis back to key

--- test/integration/game_flows_test.rb
+++ test/integration/game_flows_test.rb
@@ -17,13 +17,13 @@ class GameFlowsTest < ActionDispatch::IntegrationTest
     }
     post games_path, as: :json, headers: {token: @token}, params: input
     response = ActiveSupport::JSON.decode @response.body
-    assert response.key? 'game', "Game should be assigned"
+    assert response.key?('game'), "Game should be assigned"
     game = response['game']
-    assert game.key? 'id', "ID should be assigned"
-    assert game.key? 'maxPlayers', "Max players should be assigned"
-    assert game.key? 'name', "Name should be assigned"
-    assert game.key? 'type', "Type should be assigned"
-    assert game.key? 'players', "Players should be assigned"
+    assert game.key?('id'), "ID should be assigned"
+    assert game.key?('maxPlayers'), "Max players should be assigned"
+    assert game.key?('name'), "Name should be assigned"
+    assert game.key?('type'), "Type should be assigned"
+    assert game.key?('players'), "Players should be assigned"
     assert_equal input.name, game.name, "Name should be correct"
     assert_equal input.type, game.type, "Type should be correct"
     assert_equal input.player, game.players[0].name, "Player name should be correct"
@@ -41,11 +41,11 @@ class GameFlowsTest < ActionDispatch::IntegrationTest
     assert_equal users(:cerisa).games.length, games.length, "Result count should be correct"
     for game in games
       expected = Game.find_by(id: game.id)
-      assert game.key? 'id', "ID should be assigned"
-      assert game.key? 'maxPlayers', "Max players should be assigned"
-      assert game.key? 'name', "Name should be assigned"
-      assert game.key? 'type', "Type should be assigned"
-      assert game.key? 'players', "Players should be assigned"
+      assert game.key?('id'), "ID should be assigned"
+      assert game.key?('maxPlayers'), "Max players should be assigned"
+      assert game.key?('name'), "Name should be assigned"
+      assert game.key?('type'), "Type should be assigned"
+      assert game.key?('players'), "Players should be assigned"
       assert_equal expected.id, game.id, "ID should be correct"
       assert_equal expected.name, game.name, "Name should be correct"
       assert_equal expected.game_type, game.type, "Type should be correct"
