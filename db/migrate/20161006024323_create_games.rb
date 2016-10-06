a2cc52aba1c9e9ac3c3d5ece97dd3777399f989f
Cerisa <cerisa@strangerelics.com>
2016-10-05 20:13:19 -0700

    Change type name to something that won't break
    Apparently, "type" is a reservered column name. Oops

--- db/migrate/20161006024323_create_games.rb
+++ db/migrate/20161006024323_create_games.rb
@@ -1,10 +1,10 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     enable_extension 'uuid-ossp'
-    
+
     create_table :games, id: :uuid do |t|
       t.string :name, null: false
-      t.integer :type, null: false, default: 0
+      t.integer :game_type, null: false, default: 0
       t.integer :max_players, null: false
       t.timestamps
     end
