23d505d3c84b0ea5bcf830ff20e1a5fbca77bd5a
Cerisa <cerisa@strangerelics.com>
2016-10-05 20:20:16 -0700

    Make max_players not require null

--- db/migrate/20161006024323_create_games.rb
+++ db/migrate/20161006024323_create_games.rb
@@ -5,7 +5,7 @@ class CreateGames < ActiveRecord::Migration[5.0]
     create_table :games, id: :uuid do |t|
       t.string :name, null: false
       t.integer :game_type, null: false, default: 0
-      t.integer :max_players, null: false
+      t.integer :max_players
       t.timestamps
     end
   end
