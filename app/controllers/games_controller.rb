0e8fe3456b8903ba74777f166b43b8af396766ac
Cerisa <cerisa@strangerelics.com>
2016-10-06 16:32:07 -0700

    Add index and create to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,15 @@
 class GamesController < ApplicationController
+  include ErrorHelper
+
   def index
+    @games = @user.games 
   end
 
   def create
+    name = required params, :name
+    game_type = required params, :type
+    player = required params, :player
+    @game = Game.create name: name, game_type: game_type
+    player = Player.create name: player, game: @game, user: @user
   end
 end
