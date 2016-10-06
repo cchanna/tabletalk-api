8143ca00769039b02b77bf1250f2aeadf8ffaa34
Cerisa <cerisa@strangerelics.com>
2016-10-05 20:08:46 -0700

    Add game controller

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,7 @@
+class GameController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post :login, to: 'user#login'
   post :logout, to: 'user#logout'
+
+  resource :game, only: [:index, :create]
 end
