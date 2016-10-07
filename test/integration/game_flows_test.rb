cb2e5c117afcb8c09123e096855b5cd6afdcd863
Cerisa <cerisa@strangerelics.com>
2016-10-06 17:08:35 -0700

    Fix .value to ['value']

--- test/integration/game_flows_test.rb
+++ test/integration/game_flows_test.rb
@@ -24,34 +24,37 @@ class GameFlowsTest < ActionDispatch::IntegrationTest
     assert game.key?('name'), "Name should be assigned"
     assert game.key?('type'), "Type should be assigned"
     assert game.key?('players'), "Players should be assigned"
-    assert_equal input.name, game.name, "Name should be correct"
-    assert_equal input.type, game.type, "Type should be correct"
-    assert_equal input.player, game.players[0].name, "Player name should be correct"
-    assert_equal users(:cerisa).id, game.players[game.me].user.id, "Me should be me"
+    assert_equal input[:name], game['name'], "Name should be correct"
+    assert_equal input[:type], game['type'], "Type should be correct"
+    assert_equal input[:player], game['players'][0]['name'], "Player name should be correct"
+    assert_equal 0, game['me'], "Me should be 0"
 
     get games_path, as: :json, headers: {token: @token}
     response = ActiveSupport::JSON.decode @response.body
-    assert_equal games_count + 1, response.games.length, "Games count should be one higher"
+    assert response.key?('games'), "Games should be assigned"
+    assert_equal games_count + 1, response['games'].length, "Games count should be one higher"
   end
 
   test "view games" do
     get games_path, as: :json, headers: {token: @token}
     response = ActiveSupport::JSON.decode @response.body
-    games = response.games
+    assert response.key?('games'), "Games should be assigned"
+    games = response['games']
     assert_equal users(:cerisa).games.length, games.length, "Result count should be correct"
     for game in games
-      expected = Game.find_by(id: game.id)
+      expected = Game.find_by(id: game['id'])
       assert game.key?('id'), "ID should be assigned"
       assert game.key?('maxPlayers'), "Max players should be assigned"
       assert game.key?('name'), "Name should be assigned"
       assert game.key?('type'), "Type should be assigned"
       assert game.key?('players'), "Players should be assigned"
-      assert_equal expected.id, game.id, "ID should be correct"
-      assert_equal expected.name, game.name, "Name should be correct"
-      assert_equal expected.game_type, game.type, "Type should be correct"
-      assert_equal expected.players.length, game.players.length, "Players count should be correct"
-      assert_equal expected.max_players, game.maxPlayers, "Max players should be correct"
-      assert_equal users(:cerisa).id, game.players[game.me].user.id, "Me should be me"
+      assert_equal expected.id, game['id'], "ID should be correct"
+      assert_equal expected.name, game['name'], "Name should be correct"
+      assert_equal expected.game_type, game['type'], "Type should be correct"
+      assert_equal expected.players.length, game['players'].length, "Players count should be correct"
+      assert_equal expected.max_players, game['maxPlayers'], "Max players should be correct"
+      expected_name = users(:cerisa).players.find_by(game_id: game['id'])
+      assert_equal expected_name, game.players[game.me].name, "Me should be me"
     end
     game = response[0]
   end
