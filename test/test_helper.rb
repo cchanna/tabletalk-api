fb0e03bada89afa34d9c4eb75a78cc5df65c37af
cchanna <cerisa@strangerelics.com>
2017-01-11 15:03:04 -0800

    Add backtrace silencer

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-Minitest::Reporters.use!
+Minitest::Reporters.use!(
+  Minitest::Reporters::DefaultReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
   # Add more helper methods to be used by all tests here...
 end
