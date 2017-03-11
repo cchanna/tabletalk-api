46301ccad108c27fff8fc00915f7d3beeaf57923
cchanna <cerisa@strangerelics.com>
2017-03-11 12:08:15 -0800

    Fix migration

--- db/migrate/20170311195538_drop_blades_veteran_crew_abilities.rb
+++ db/migrate/20170311195538_drop_blades_veteran_crew_abilities.rb
@@ -2,7 +2,7 @@ class DropBladesVeteranCrewAbilities < ActiveRecord::Migration[5.0]
   def change
     add_column :blades_crew_abilities, :veteran, :boolean, null: false, default: false
     Blades::CrewAbility.all.each do |a|
-      if a.veteran_ability
+      if a.respond_to? :veteran_ability and a.veteran_ability
         a.update name: a.veteran_ability.name, veteran: true
       end
     end
